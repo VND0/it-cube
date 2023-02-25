719770d043236928320521af4c918b89e345cda2
CUBER <vnd-08@yandex.ru>
2023-02-25 19:46:32 +0300

    я хз, что поменялось, но вроде работает. как и прежде

--- rad_btn/task.py
+++ rad_btn/task.py
@@ -1,30 +1,48 @@
 from tkinter import *
 from tkinter.ttk import Radiobutton
+from tkinter import messagebox
+
+less_than_zero = "Длина стороны/сторон меньше нуля"
+not_a_digit = "Длина стороны/сторон не является целым числом"
+more_than_fifteen = "Длина стороны/сторон больше 15"
+tit = "Ошибка данных"
 
 
 def print_to_text():
+    width, height = None, None
     if int(variant.get()):
         width = int(variant.get())
         height = int(variant.get())
     else:
-        width = int(en_w.get())
-        height = int(en_h.get())
+        try:
+            width = int(en_w.get())
+            height = int(en_h.get())
+        except ValueError:
+            messagebox.showerror(title=tit, message=not_a_digit)
+
+    if width < 0 or height < 0:
+        messagebox.showerror(title=tit, message=less_than_zero)
+        0/0
+    elif width > 15 or height > 15:
+        messagebox.showerror(tit, more_than_fifteen)
+        0/0
 
     field.configure(state=NORMAL)
     field.delete(0.0, END)
     for i in range(height):
         if i == 0 or i == height-1:
-            field.insert(float(i), "#" * width + "\n")
+            field.insert(END, "#" * width + "\n")
         else:
-            field.insert(float(i), ("#" + " " * (width-2) + "#" + "\n"))
+            field.insert(END, ("#" + " " * (width-2) + "#" + "\n"))
     field.configure(state=DISABLED)
 
 
 root = Tk()
 root.title("Generate a square")
-glob_ft = ("Segoe UI", 15)
+root.geometry("500x500")
+glob_ft = ("Courier New", 15)
 
-field = Text(root, width=30, height=30, font=glob_ft, state=DISABLED)
+field = Text(root, width=30, height=15, state=DISABLED, font=glob_ft)
 
 var4x4 = "4 x 4"
 var5x5 = "5 x 5"
@@ -36,13 +54,18 @@ btn5 = Radiobutton(root, text=var5x5, value=5, variable=variant, command=print_t
 btn6 = Radiobutton(root, text=var6x6, value=6, variable=variant, command=print_to_text)
 
 lbl = Label(root, text="x")
-en_w = Entry(root, width=2, font=glob_ft)
-en_h = Entry(root, width=2, font=glob_ft)
+en_w = Entry(root, width=2)
+en_h = Entry(root, width=2)
 custom = Radiobutton(root, value=0, variable=variant, command=print_to_text)
 
 field.grid(row=0, column=0, padx=5, pady=5, rowspan=10, columnspan=10)
-btn4.grid(row=0, column=11, padx=5, pady=5)
-btn5.grid(row=1, column=11, padx=5, pady=5)
-btn6.grid(row=2, column=11, padx=5, pady=5)
+btn4.grid(row=0, column=11, padx=5, pady=5, sticky=W)
+btn5.grid(row=1, column=11, padx=5, pady=5, sticky=W)
+btn6.grid(row=2, column=11, padx=5, pady=5, sticky=W)
+
+custom.grid(row=3, column=11, padx=5, pady=5, sticky=W)
+en_w.grid(row=4, column=11, padx=0, pady=0, sticky=W)
+lbl.grid(row=4, column=11, padx=0, pady=0)
+en_h.grid(row=4, column=11, padx=0, pady=0, sticky=E)
 
 root.mainloop()
